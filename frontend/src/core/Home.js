4fae2f62db39437def643bfd6f65ef0f5d34bf52
adil khan <blueaadil@gmail.com>
2023-05-26 15:56:22 +0530

    minor cosmetic fixes

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -2,7 +2,7 @@ import React,{useState,useEffect} from 'react'
 import Base from "./Base"
 import Card from "./Card"
 import NavBar from "./NavBar"
-
+import logo from "./logo.png";
 import ProductDetails from "./ProductDetails"
 import {getProducts} from "./helper/coreapicalls"
 
@@ -66,7 +66,7 @@ function Hero(){
             <div>
                 <p className="beats-solo">Hero Text</p>
                 <h3>Mid Text</h3>
-                <img src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80" className="hero-banner-image" style={{width:"100%"}} />
+                <img src={"https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3303&q=80"} className="hero-banner-image" style={{width:"100%"}} />
 
             </div>
         </div>
